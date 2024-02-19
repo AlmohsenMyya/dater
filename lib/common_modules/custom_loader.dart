f309792646b2560b551594d0f0383a6f8a219487
almohsen myya <almohsenmyya2233@@gmail.com>
2024-02-19 17:53:25 -0500

    update CustomLoader

--- lib/common_modules/custom_loader.dart
+++ lib/common_modules/custom_loader.dart
@@ -4,20 +4,20 @@ import 'package:get/get.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class CustomLoader extends StatelessWidget {
-  const CustomLoader({Key? key}) : super(key: key);
+  final double size;
+
+  const CustomLoader({Key? key, this.size = -1.0}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final loaderSize = size != -1.0 ? size : Get.height * 0.07;
+
     return Center(
       child: LoadingAnimationWidget.staggeredDotsWave(
         color: AppColors.lightOrangeColor,
-        size: Get.height * 0.07,
+        size: loaderSize,
       ),
     );
-    /*return const Center(
-      child: CircularProgressIndicator(
-        color: AppColors.darkOrangeColor,
-      ),
-    );*/
   }
 }
+
